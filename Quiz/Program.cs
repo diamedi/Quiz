c3bb6860190d2e2e2b76199e0dc80b46f557f16d
Roderick <roderickmobiel@gmail.com>
2019-12-03 13:28:58 +0100

    Test Push

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -6,7 +6,7 @@ namespace Quiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
         }
     }
 }
